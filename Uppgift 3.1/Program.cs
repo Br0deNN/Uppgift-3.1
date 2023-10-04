00f89875cce4a136ed8b07b4bda702c428fb8dd2
7R1XY <theo.broden@gmail.com>
2023-10-04 10:23:20 +0200

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if ( ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+
+            else if (ålder < 16) 
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
